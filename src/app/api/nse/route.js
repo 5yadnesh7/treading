de31c28c438e58c8414141d762bb8b6f04d601ef
Yadnesh <gaikwadyadnesh@gmail.com>
2023-07-31 13:08:32 +0530

    changes

--- src/app/api/nse/route.js
+++ src/app/api/nse/route.js
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import axios from "axios";
 
-export const dynamic = 'force-dynamic'
-
 export async function GET(request, context) {
   try {
     const { searchParams } = new URL(request.url);
