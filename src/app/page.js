59130efa7033bb3844f7ad5c448a64412f2d7a54
Yadnesh <gaikwadyadnesh@gmail.com>
2023-08-10 13:20:45 +0530

    new signal added

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ const Home = () => {
   const [opData, setOpData] = useState({ poi: 0, coi: 0, data: [], changedCE: 0, changedPE: 0, oiGraph: { call: [], put: [] }, oiChangedGraph: { call: [], put: [] }, strikePriceAry: [] });
   const [selectedOpType, setSelectedOpType] = useState("NIFTY");
   const [columnNumber, setColumnNumber] = useState(5)
+  const [buySellTotal, setBuySellTotal] = useState({ CBuy: 0, CSell: 0, PBuy: 0, PSell: 0 })
 
   useEffect(() => {
     fetchNseData(selectedOpType);
@@ -45,6 +46,10 @@ const Home = () => {
         const putOiAry = []
         const callOiChangedAry = []
         const putOiChangedAry = []
+        let callBuy = 0;
+        let callSell = 0;
+        let putBuy = 0;
+        let putSell = 0;
         let currentStrikePrice = ""
         data.map((item) => {
           if (!currentStrikePrice) {
@@ -53,11 +58,18 @@ const Home = () => {
           changedCE += item.CE.changeinOpenInterest;
           changedPE += item.PE.changeinOpenInterest;
 
+          callBuy += item.CE.totalBuyQuantity;
+          callSell += item.CE.totalSellQuantity;
+          putBuy += item.PE.totalBuyQuantity;
+          putSell += item.PE.totalSellQuantity;
+
           callOiAry.push({ label: item.CE.strikePrice, y: item.CE.openInterest })
           putOiAry.push({ label: item.PE.strikePrice, y: item.PE.openInterest })
           callOiChangedAry.push({ label: item.CE.strikePrice, y: item.CE.changeinOpenInterest })
           putOiChangedAry.push({ label: item.PE.strikePrice, y: item.PE.changeinOpenInterest })
         });
+
+        setBuySellTotal({ CBuy: callBuy, CSell: callSell, PBuy: putBuy, PSell: putSell })
 
         const formattedCallOiAry = filterByStrikePrice(callOiAry, currentStrikePrice, columnCount);
         const formattedPutOiAry = filterByStrikePrice(putOiAry, currentStrikePrice, columnCount);
@@ -84,6 +96,9 @@ const Home = () => {
   const changedPutCallRation = (changePE / changeCE).toFixed(4);
   const downtrend = putCallRation >= 1.7 || (putCallRation > 0.7 && putCallRation <= 1);
   const uptrend = putCallRation <= 0.7 || (putCallRation > 1 && putCallRation <= 1.7);
+  const CPBR = (buySellTotal.CBuy / buySellTotal.PBuy).toFixed(4)
+  const PCSR = (buySellTotal.PSell / buySellTotal.CSell).toFixed(4)
+  const BSAdd = (buySellTotal.CBuy + buySellTotal.PSell) + (buySellTotal.PBuy + buySellTotal.CSell)
 
   return (
     <div className={styles["home-container"]}>
@@ -98,23 +113,6 @@ const Home = () => {
               <div className={styles["callPutRatio"]}>
                 PCR:- {putCallRation}
               </div>
-            </div>
-            <div>
-              <select
-                className={styles["optionType"]}
-                onChange={(e) => {
-                  fetchNseData(e.target.value, columnNumber);
-                  setSelectedOpType(e.target.value);
-                }}
-              >
-                {optionType.map((item) => {
-                  return (
-                    <option key={item.symbol} value={item.symbol}>
-                      {item.name}
-                    </option>
-                  );
-                })}
-              </select>
             </div>
             <div className={styles["changedOi-container"]}>
               <div className={styles["putOI"]}>
@@ -140,20 +138,103 @@ const Home = () => {
                 </label>
               </div>
             </div>
+            <div>
+              <select
+                className={styles["optionType"]}
+                onChange={(e) => {
+                  fetchNseData(e.target.value, columnNumber);
+                  setSelectedOpType(e.target.value);
+                }}
+              >
+                {optionType.map((item) => {
+                  return (
+                    <option key={item.symbol} value={item.symbol}>
+                      {item.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className={styles["changedOi-container"]}>
+              <div>
+                Call Buy:- {buySellTotal.CBuy}
+              </div>
+              <div>
+                Put Buy:- {buySellTotal.PBuy}
+              </div>
+              <div>
+                CPBR:- {CPBR}
+              </div>
+              <div className={styles["trend"]}>
+                Trend Chance:-
+                <label className={`${CPBR < 1 && styles["down"]} ${CPBR >= 1 && styles["up"]}`}>
+                  {CPBR < 1 ? "Down" : CPBR > 1 ? "Up" : "Up"}
+                </label>
+              </div>
+            </div>
+            <div className={styles["changedOi-container"]}>
+              <div>
+                Put Sell:- {buySellTotal.PSell}
+              </div>
+              <div>
+                Call Sell:- {buySellTotal.CSell}
+              </div>
+              <div>
+                PCSR:- {PCSR}
+              </div>
+              <div className={styles["trend"]}>
+                Trend Chance:-
+                <label className={`${PCSR < 1 && styles["down"]} ${PCSR >= 1 && styles["up"]}`}>
+                  {PCSR < 1 ? "Down" : PCSR > 1 ? "Up" : "Up"}
+                </label>
+              </div>
+            </div>
+            <div className={styles["changedOi-container"]}>
+              <div>
+                Call Buy:- {buySellTotal.CBuy}
+              </div>
+              <div>
+                Put Sell:- {buySellTotal.PSell}
+              </div>
+              <div>
+                Total Calc:- {buySellTotal.CBuy + buySellTotal.PSell}
+              </div><br />
+              <div>
+                Put Buy:- {buySellTotal.PBuy}
+              </div>
+              <div>
+                Call Sell:- {buySellTotal.CSell}
+              </div>
+              <div>
+                Total Calc:- {buySellTotal.PBuy + buySellTotal.CSell}
+              </div><br />
+              <div className={styles["trend"]}>
+                Go with:-
+                <label className={`${BSAdd < 1 && styles["down"]} ${BSAdd >= 1 && styles["up"]}`}>
+                  {BSAdd > 1 ? " Call" : " Put"}
+                </label> Buy
+              </div>
+            </div>
           </div>
           <table border={1} className={styles["table-container"]}>
             <thead className={styles["header"]}>
               <tr className={styles["no-border"]}>
-                <th colSpan={3}>Call</th>
+                <th colSpan={6}>Call</th>
                 <th></th>
-                <th colSpan={3}>Put</th>
+                <th colSpan={6}>Put</th>
               </tr>
               <tr>
                 <th>OI</th>
                 <th>Changed in OI</th>
+                <th>Total Buying Qty</th>
+                <th>Total Selling Qty</th>
+                <th>BSR</th>
                 <th>Price</th>
                 <th>Strike Price</th>
                 <th>Price</th>
+                <th>BSR</th>
+                <th>Total Selling Qty</th>
+                <th>Total Buying Qty</th>
                 <th>Changed in OI</th>
                 <th>OI</th>
               </tr>
@@ -166,14 +247,20 @@ const Home = () => {
                   return (
                     <>
                       <tr key={`option-strike`} className={styles["atTheMoney"]}>
-                        <td colSpan={7}>{item.CE.underlyingValue}</td>
+                        <td colSpan={14}>{item.CE.underlyingValue}</td>
                       </tr>
                       <tr key={`option ${ind}`}>
                         <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.openInterest}</td>
                         <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]} ${item.CE.changeinOpenInterest < 0 ? styles["danger"] : ""}`}>{item.CE.changeinOpenInterest}</td>
+                        <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.totalBuyQuantity}</td>
+                        <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.totalSellQuantity}</td>
+                        <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{(item.CE.totalBuyQuantity / item.CE.totalSellQuantity).toFixed(4)}</td>
                         <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.askPrice}</td>
                         <td className={styles["strikPrice"]}>{item.CE.strikePrice}</td>
                         <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.askPrice}</td>
+                        <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{(item.PE.totalBuyQuantity / item.PE.totalSellQuantity).toFixed(4)}</td>
+                        <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.totalSellQuantity}</td>
+                        <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.totalBuyQuantity}</td>
                         <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]} ${item.PE.changeinOpenInterest < 0 ? styles["danger"] : ""}`}>{item.PE.changeinOpenInterest}</td>
                         <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.openInterest}</td>
                       </tr>
@@ -184,9 +271,15 @@ const Home = () => {
                   <tr key={`option ${ind}`}>
                     <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.openInterest}</td>
                     <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]} ${item.CE.changeinOpenInterest < 0 ? styles["danger"] : ""}`}>{item.CE.changeinOpenInterest}</td>
+                    <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.totalBuyQuantity}</td>
+                    <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.totalSellQuantity}</td>
+                    <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{(item.CE.totalBuyQuantity / item.CE.totalSellQuantity).toFixed(4)}</td>
                     <td className={`${callInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.CE.askPrice}</td>
                     <td className={styles["strikPrice"]}>{item.CE.strikePrice}</td>
                     <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.askPrice}</td>
+                    <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{(item.PE.totalBuyQuantity / item.PE.totalSellQuantity).toFixed(4)}</td>
+                    <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.totalSellQuantity}</td>
+                    <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.totalBuyQuantity}</td>
                     <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]} ${item.PE.changeinOpenInterest < 0 ? styles["danger"] : ""}`}>{item.PE.changeinOpenInterest}</td>
                     <td className={`${putInTheMoney ? styles["inTheMoney"] : styles["outTheMoney"]}`}>{item.PE.openInterest}</td>
                   </tr>
